4636c2764219330ce4c15ca9eca8708dbcbb35a2
suhyun <suhyuuuuuun@gmail.com>
2022-09-19 14:19:13 +0900

    Update

---
+++ jsdemo/js/a.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+  console.log("Hello");
+  let data = document.getElementById("wrap");
+  console.log(data.innerText);
+};
+
+//옛날방식
+// <script type="text/javascript" src="js/b.js"></script>
+
+// $(document), ready(function(){});
---
+++ jsdemo/js/b.js
@@ -0,0 +1,8 @@
+//document.write("Hello");
+console.log("Hello");
+let data = document.getElemnetById("wrap");
+//document.write(data.innerText);
+console.log(data.innerText);
+
+//현재방식
+// <script defer type="text/javascript" src="js/b.js"></script>
