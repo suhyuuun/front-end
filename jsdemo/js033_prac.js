432910044bc852fb0186c3788c3ddbc6e5ef441e
suhyun <suhyuuuuuun@gmail.com>
2022-09-23 18:16:17 +0900

    Update

--- jsdemo/js033_prac.js
+++ jsdemo/js033_prac.js
@@ -21,11 +21,31 @@ let arr = [1, 2, 3, undefined, 4, undefined, undefined, 5];
 
   
  <출력결과]
- [[ '홍길동', 90, 85, 40, 215, 71.67 ],
-  [ '이영희', 100, 35, 75, 210, 70.00 ]]
+ [[ '홍길동', 90, 85, 40, 215, '71.67' ],
+  [ '이영희', 100, 35, 75, 210, '70.00' ]]
   최고점: 71.67
 */
 let data = [
   ["홍길동", 90, 85, 40],
   ["이영희", 100, 35, 75],
 ];
+let listData = [];
+let average1;
+let average2;
+
+function getSum(total, num) {
+  return total + num;
+}
+for (let i = 0; i < data.length; i++) {
+  let j = data[i];
+  let k = data[i].slice(1);
+  let l = k.reduce(getSum);
+  let m = (l / 3).toFixed(2);
+  listData.push(m);
+}
+average1 = listData[0];
+average2 = listData[1];
+let arr1 = [...data[0], average1];
+let arr2 = [...data[1], average2];
+console.log(arr1);
+console.log(arr2);
